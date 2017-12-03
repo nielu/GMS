c12b7094950121409a6ce2c99f125053582e9eb5
nielu <jan.nielek@gmail.com>
2017-12-03 11:44:12 +0100

    add original BellmanFord solution

--- BellmanFord/Program.cs
+++ BellmanFord/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace BellmanFord
 {
@@ -7,53 +6,75 @@ namespace BellmanFord
     {
         static void Main(string[] args)
         {
-            var app = new App();
-            app.Run();
-
+            var tests = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tests; ++i)
+            {
+                var g = Graph.Read();
+                g.BellmanFord();
+                Console.WriteLine("");
+            }
+            //Console.ReadLine();
         }
     }
 
-
-    class App
-    {
-        public void Run()
-        {
-            var graphs = readInput();
-
-        }
-
-        private List<Graph> readInput()
-        {
-            var list = new List<Graph>();
-            var n = Int32.Parse(Console.ReadLine());
-            for (int i = 0; i < n; ++i)
-                list.Add(Graph.ReadFromStdin());
-            return list;
-        }
-
-
-
-    }
-
-    struct Graph
+    class Graph
     {
         int size;
-        int[,] weights;
+        int[,] vertices;
 
-        public static Graph ReadFromStdin()
+
+        public static Graph Read()
         {
             var g = new Graph();
-
-            g.size = Int32.Parse(Console.ReadLine());
-            g.weights = new int[g.size, g.size];
+            g.size = int.Parse(Console.ReadLine());
+            g.vertices = new int[g.size, g.size];
 
             for (int i = 0; i < g.size; ++i)
             {
-                var weights = Console.ReadLine().Split(' ');
+                var line = Console.ReadLine().Split(' ');
                 for (int j = 0; j < g.size; ++j)
-                    g.weights[i, j] = Int32.Parse(weights[j]);
+                {
+                    g.vertices[i, j] = int.Parse(line[j]);
+                }
             }
+
             return g;
+        }
+
+        public void BellmanFord()
+        {
+            var d = new int[size];
+            var i = 0;
+            d[0] = 0;
+
+            for (i = 1; i < size; ++i)
+                d[i] = Int32.MaxValue;
+
+            for (i = 1; i < size; ++i)
+            {
+                for (int u = 0; u < size; ++u)
+                {
+                    for (int v = 0; v < size; ++v)
+                    {
+                        if (vertices[u, v] == 0 || d[u] == Int32.MaxValue)
+                            continue;
+
+
+                        if (d[u] + vertices[u, v] < d[v])
+                            d[v] = d[u] + vertices[u, v];
+                    }
+                }
+
+                for (int j = 0; j < size; ++j)
+                {
+                    if (d[j] == Int32.MaxValue)
+                        Console.Write("0 ");
+                    else
+                        Console.Write($"{d[j]} ");
+                }
+                Console.WriteLine("");
+
+            }
         }
     }
 }
